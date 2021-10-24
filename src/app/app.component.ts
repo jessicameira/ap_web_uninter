c737ee620db0a1d9bbb7497cc14d2aec2ac5eb85
Jessica Meira <jessica.meira1@gmail.com>
2021-10-23 21:57:43 -0300

    adicionando arquivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { alunos } from './arquivo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'atividade-pratica';
+
+  lista_alunos = alunos;
 }
---
+++ src/app/arquivo.ts
@@ -0,0 +1,32 @@
+export const alunos = [
+    {
+    ru: 801194,
+    nome: "Jessica Leandro de Meira",
+    curso: "Analise e desenvolvimento de sistemas",
+    dt_nasc: "03/05/1989",
+    },
+    {
+    ru: 102343,
+    nome: "Aluno Teste",
+    curso: "Engenharia de software",
+    dt_nasc: "13/06/2000",
+    },
+    {
+    ru: 334456,
+    nome: "Aluno Simulado ",
+    curso: "Administração",
+    dt_nasc: "10/11/1997",
+    },
+    {
+    ru: 656544,
+    nome: "Alundo de Souza",
+    curso: "Pedagogia",
+    dt_nasc: "17/04/1999",
+    },
+    {
+    ru: 990099,
+    nome: "Aluno dos Santos",
+    curso: "Engenharia da computação",
+    dt_nasc: "11/02/1999",
+    }
+    ];
